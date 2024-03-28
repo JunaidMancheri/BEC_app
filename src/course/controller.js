80934a6d44028f3679af4df2b24fc9a8bead4c74
Junaid <junaidofficialnow@gmail.com>
2024-03-29 01:28:25 +0530

    [course] fix: course  duplication bug fixed

--- src/course/controller.js
+++ src/course/controller.js
@@ -1,43 +1,63 @@
-const { EventEmitter } = require("../common/EventEmitter");
-const { respondSuccess } = require("../common/response.helper");
-const { CourseModel } = require("./model")
+const {EventEmitter} = require('../common/EventEmitter');
+const {respondSuccess} = require('../common/response.helper');
+const {generateSlug} = require('../common/slug.helper');
+const {CourseModel} = require('./model');
+const {Conflict} = require('http-errors');
 
-exports.createCourse = async (req, res) =>  {
-  const doc = await CourseModel.create({
-    description: req.body.description,
-    name: req.body.name,
-    duration: Number(req.body.duration),
-    type: req.body.type,
-  })
-
+exports.createCourse = async (req, res) => {
+  let doc;
+  try {
+    doc = await CourseModel.create({
+      description: req.body.description,
+      name: req.body.name,
+      nameSlug: generateSlug(req.body.name),
+      duration: Number(req.body.duration),
+      type: req.body.type,
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      throw new Conflict('Course with this name already exists');
+    }
+    throw error;
+  }
   res.json(respondSuccess(doc)).status(201);
-}
+};
 
 exports.getCourses = async (req, res) => {
   const courses = await CourseModel.find();
   res.json(respondSuccess(courses));
-}
+};
 
-
-// check if we can combine this to single db call;
 exports.updateCourse = async (req, res) => {
-  if (Object.keys(req.body).length ==0) {
+  if (Object.keys(req.body).length == 0) {
     return res.status(204).end();
   }
 
-  const doc = await CourseModel.findById(req.params.courseId);
+  const updates = {};
 
-  if (req.body.name) doc.name = req.body.name;
-  if (req.body.description) doc.description = req.body.description;
-  if (req.body.duration) doc.duration = Number(req.body.duration);
-  if (req.body.type) doc.type = req.body.type;
-
-  await doc.save();
+  if (req.body.name) {
+    updates.name = req.body.name;
+    updates.nameSlug = generateSlug(req.body.name);
+  }
+  if (req.body.description) updates.description = req.body.description;
+  if (req.body.duration) updates.duration = Number(req.body.duration);
+  if (req.body.type) updates.type = req.body.type;
+  let doc;
+  try {
+    doc = await CourseModel.findByIdAndUpdate(req.params.courseId, updates, {
+      new: true,
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      throw new Conflict('Course with this name already exists');
+    }
+    throw error;
+  }
   res.json(respondSuccess(doc));
-}
+};
 
 exports.deleteCourse = async (req, res) => {
   await CourseModel.findByIdAndDelete(req.params.courseId);
   EventEmitter.emit('Course:Deleted', {courseId: req.params.courseId});
   res.status(204).end();
-}
+};
--- src/course/model.js
+++ src/course/model.js
@@ -2,6 +2,7 @@ const {Schema, model} = require('mongoose');
 
 const courseSchema = new Schema({
   name: {type: String, required: true},
+  nameSlug : {type: String, required: true, unique: true},
   description: {type: String, required: true},
   duration: {type: Number, required: true},
   type: {type: String, enum: ['undergraduate, postgraduate'], required: true},
