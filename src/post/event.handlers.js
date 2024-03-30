cf8b03c74e3965af12e1da4981ea6dc080833cdb
Junaid <junaidofficialnow@gmail.com>
2024-03-30 23:48:22 +0530

    [post] feat: listened to events from course  deleted and amenity deleted events

--- src/post/event.handlers.js
+++ src/post/event.handlers.js
@@ -1,9 +1,26 @@
 const {EventEmitter} = require('../common/EventEmitter');
 const {PostModel} = require('./post.model');
 
-EventEmitter.on('Category:StatusChanged', async data => {
-  await PostModel.updateMany(
+EventEmitter.on('Category:StatusChanged', data => {
+ PostModel.updateMany(
     {category: data.categoryId},
     {isCategoryActive: data.status}
-  );
+  ).catch(()=>{});
 });
+
+
+EventEmitter.on('Amenity:Deleted', data => {
+  PostModel.updateMany({
+    $pull: {
+      amenities: data.amenityId
+    }
+  }).catch(()=>{});
+} )
+
+EventEmitter.on('Course:Deleted', data => {
+  PostModel.updateMany({
+    $pull: {
+      courses: data.courseId
+    }
+  }).catch(()=>{});
+}) 
