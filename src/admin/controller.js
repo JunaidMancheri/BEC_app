59ea9d2b7a49e232612f8b34f84f41a0d2307b23
Junaid <junaidofficialnow@gmail.com>
2024-03-31 15:40:24 +0530

    [admin]  feat: implemented  logger

--- src/admin/controller.js
+++ src/admin/controller.js
@@ -4,12 +4,18 @@ const {AdminModel} = require('./model');
 const {generateAndStoreToken} = require('../auth/token.service');
 const {Conflict} = require('http-errors');
 const { addToBlockList, removeFromBlockList } = require('../auth');
+const { makeLogger } = require('../common/logger.config');
+
+const Logger = makeLogger('Admin');
 
 exports.inviteAdmin = async (req, res) => {
   const admin = await AdminModel.findOne({email: req.body.email});
   if (admin) throw new Conflict('admin  already exists');
   const token = await generateAndStoreToken(req.body.email);
   await sendInvitationMail(req.body.email, token);
+
+
+  Logger.info('Invited  admin ' + req.body.email);
   res.json(respondSuccess({message: 'Invitation mail send successfully'}));
 };
 
@@ -21,6 +27,8 @@ exports.getAdmins = async (req, res) => {
 exports.deleteAdmin = async (req, res) => {
   await AdminModel.findById(req.params.adminId);
   addToBlockList(req.params.adminId);
+
+  Logger.info('Deleted admin ' + req.params.adminId);
   res.status(204).end();
 };
 
@@ -29,5 +37,6 @@ exports.toggleBlock = async (req, res) => {
   admin.isBlocked = !admin.isBlocked;
   admin.isBlocked ? addToBlockList(admin._id) : removeFromBlockList(admin._id);
   await admin.save();
+  Logger.info('Changed admin  status  to ' + admin.isBlocked + ' ' + admin.email);
   res.json(respondSuccess(admin));
 };
