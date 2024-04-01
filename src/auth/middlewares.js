29886d40dcf34239b7be7ba84296f92b426dcbbd
Junaid <junaidofficialnow@gmail.com>
2024-04-01 07:19:38 +0530

    fix: bug  fixed on the populate user details  middleware

--- src/auth/middlewares.js
+++ src/auth/middlewares.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { appConfig } = require('../config/app.config');
+const {Unauthorized} = require('http-errors');
 
 exports.populateUserDetails = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1]
@@ -7,8 +8,8 @@ exports.populateUserDetails = (req, res, next) => {
   try {
     const decodedToken = jwt.verify(token, appConfig.JWT_SECRET_KEY);
     req.user = decodedToken
-  } catch (error) {}
-  finally {
     next();
+  } catch (error) {
+    throw new Unauthorized('Invalid jwt or token expired');
   }
 }
