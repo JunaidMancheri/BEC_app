ffbf3c7e71dc57b6c40c3cc887985bf2ccee26c6
Junaid <junaidofficialnow@gmail.com>
2024-04-01 05:30:39 +0530

    fix: added badRequest error for empy query

--- src/search/index.js
+++ src/search/index.js
@@ -1,12 +1,13 @@
 const {respondSuccess} = require('../common/response.helper');
 const {searchCourses} = require('../course/course.service');
 const {searchPosts} = require('../post');
+const {BadRequest} = require('http-errors');
 
 exports.searchPosts = async (req, res) => {
-  if (!req.query.q) return res.status(204).json(respondSuccess({posts: [], courses: []})).end();
+  if (!req.query.query) throw new BadRequest('Query missing, provide query for search');
   const results = await Promise.all([
-    searchPosts(req.query.q),
-    searchCourses(req.query.q),
+    searchPosts(req.query.query),
+    searchCourses(req.query.query),
   ]);
   res.json(respondSuccess({posts: results[0], courses: results[1]})).end();
 };
