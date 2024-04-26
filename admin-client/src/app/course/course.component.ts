b55bc47978e6d56737945a8e221d793855ed9607
Junaid <junaidofficialnow@gmail.com>
2024-04-27 03:02:34 +0530

    feat (admins-client) : implemented delete course feature

--- admin-client/src/app/course/course.component.ts
+++ admin-client/src/app/course/course.component.ts
@@ -5,6 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { SnackbarService } from '../shared/snackbar.service';
 
 export interface Course {
   _id: string;
@@ -16,20 +17,22 @@ export interface Course {
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [
-    MatTableModule,
-    MatButtonModule,
-    MatIconModule,
-    MatDialogModule,
-  ],
+  imports: [MatTableModule, MatButtonModule, MatIconModule, MatDialogModule],
   templateUrl: './course.component.html',
   styleUrl: './course.component.css',
 })
 export class CourseComponent implements OnInit {
   http = inject(HttpClient);
   matDialog = inject(MatDialog);
+  snackbarService = inject(SnackbarService);
 
-  displayedColumns: string[] = [ 'name', 'description', 'duration', 'type', 'actions'];
+  displayedColumns: string[] = [
+    'name',
+    'description',
+    'duration',
+    'type',
+    'actions',
+  ];
   dataSource: Course[] = [];
 
   ngOnInit(): void {
@@ -40,13 +43,19 @@ export class CourseComponent implements OnInit {
       });
   }
 
+  deleteCourse(id: string) {
+    this.http.delete(environment.baseUrl + '/courses/' + id).subscribe(() => {
+      this.dataSource = this.dataSource.filter((item) => item._id !== id);
+      this.snackbarService.openSnackbar('Deleted course successfully');
+    });
+  }
+
   openAddCategoryDialog() {
     // this.matDialog
     //   .open(AddCategoryFormComponent, { panelClass: 'rounded-full' })
     //   .afterClosed()
     //   .subscribe((result) => {
     //     console.log(result.get('image'));
-
     //     this.http.post(environment.baseUrl + '/categories', result).subscribe((result) => console.log(result));
     //   });
   }
