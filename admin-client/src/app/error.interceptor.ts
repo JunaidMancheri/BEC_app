275e7333c06a6b415de1a5594bb01a1ae8099431
Junaid <junaidofficialnow@gmail.com>
2024-04-28 20:39:28 +0530

    feat (admins-client) : implemented erro catching interceptor

---
+++ admin-client/src/app/error.interceptor.ts
@@ -0,0 +1,14 @@
+import {  HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { Observable, catchError, of, throwError } from "rxjs";
+import { SnackbarService } from "./shared/snackbar.service";
+
+export  function CatchHttpErrorInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+  const snackbarService = inject(SnackbarService);
+  return next(req).pipe(
+    catchError((err) => {
+      snackbarService.openSnackbar(err.error.error.message)
+      return throwError(() => err);
+    })
+  )
+}
