4776da10352a5e15a4829ee6af291ff42b11c850
Junaid <junaidofficialnow@gmail.com>
2024-04-28 20:57:52 +0530

    feat (admins-client) : implemented toggle status feature

--- admin-client/src/app/banner/banner.component.ts
+++ admin-client/src/app/banner/banner.component.ts
@@ -11,6 +11,7 @@ import { BannerFormComponent } from './banner-form/banner-form.component';
 import { SnackbarService } from '../shared/snackbar.service';
 
 export interface Banner {
+  _id: string;
   imageUrl: string;
   title: string;
   description: string;
@@ -35,13 +36,30 @@ export class BannerComponent {
   banners: Banner[] = [];
   http = inject(HttpClient);
   matDialog = inject(MatDialog);
-  snackbarService  = inject(SnackbarService);
+  snackbarService = inject(SnackbarService);
 
   ngOnInit(): void {
     this.http
       .get<{ data: Banner[] }>(environment.baseUrl + '/banners')
       .subscribe((res) => {
         this.banners = res.data;
+      });
+  }
+
+  toggleStatus(id: string) {
+    this.http
+      .patch<{ data: Banner }>(
+        environment.baseUrl + '/banners/' + id + '/toggle-status',
+        {}
+      )
+      .subscribe((res) => {
+        const updateIndex = this.banners.findIndex((item) => item._id === id);
+        this.banners[updateIndex].isActive = res.data.isActive;
+        if (res.data.isActive) {
+          this.snackbarService.openSnackbar('Banner listed successfully');
+        } else {
+          this.snackbarService.openSnackbar('Banner unlisted successfully');
+        }
       });
   }
 
