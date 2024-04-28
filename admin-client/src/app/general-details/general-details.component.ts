144b565e9bf2a39a449c1aea5a6ed591cb08836b
Junaid <junaidofficialnow@gmail.com>
2024-04-29 02:40:48 +0530

    feat (admins-client)  implemented  update general details feature

--- admin-client/src/app/general-details/general-details.component.ts
+++ admin-client/src/app/general-details/general-details.component.ts
@@ -5,6 +5,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { environment } from '../../environments/environment';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 export interface GeneralDetails {
   email_1: string;
@@ -16,18 +22,49 @@ export interface GeneralDetails {
 @Component({
   selector: 'app-general-details',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './general-details.component.html',
-  styleUrl: './general-details.component.css'
+  styleUrl: './general-details.component.css',
 })
 export class GeneralDetailsComponent implements OnInit {
   http = inject(HttpClient);
-  details:GeneralDetails = {} as GeneralDetails
+  editMode = false;
+
+  detailsForm = new FormGroup({
+    email_1: new FormControl('', { validators: [Validators.required] }),
+    email_2: new FormControl('', { validators: [Validators.required] }),
+    phoneNo_1: new FormControl('', { validators: [Validators.required] }),
+    phoneNo_2: new FormControl('', { validators: [Validators.required] }),
+  });
+
   ngOnInit(): void {
-    this.http.get<{data: GeneralDetails}>(environment.baseUrl + '/general-details').subscribe(res => {
-          this.details = res.data;
-    })
+    this.http
+      .get<{ data: GeneralDetails }>(environment.baseUrl + '/general-details')
+      .subscribe((res) => {
+        this.detailsForm.disable();
+        this.detailsForm.setValue({
+          email_1: res.data.email_1,
+          email_2: res.data.email_2,
+          phoneNo_1: res.data.phoneNo_1,
+          phoneNo_2: res.data.phoneNo_2,
+        });
+      });
   }
 
+  saveDetails() {
+    this.http
+      .put(environment.baseUrl + '/general-details', this.detailsForm.value)
+      .subscribe(() => (this.detailsForm.disable(), (this.editMode = false)));
+  }
 
+  enableEditMode() {
+    this.editMode = true;
+    this.detailsForm.enable();
+  }
 }
